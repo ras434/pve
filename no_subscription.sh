c2c0f2db91c7f6be660c43e8cdd8e54b87c18bd0
ras434 <ras434@users.noreply.github.com>
2020-01-05 16:13:18 -0500

    Update no_subscription.sh

--- no_subscription.sh
+++ no_subscription.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sed -i.bak "s/data.status !== 'Active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
 cp -f /etc/apt/sources.list /etc/apt/sources.list.bak
-curl https://raw.githubusercontent.com/ras434/pve/master/sources.list > /etc/apt/sources.list
+wget -q https://raw.githubusercontent.com/ras434/pve/master/sources.list -O /etc/apt/sources.list
